0b0e263581b3fa3ecf5860328a5bc5b45130addf
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-18 14:17:42 +0500

    05

--- Index05.py
+++ Index05.py
@@ -6,46 +6,106 @@ def main(s):
     Returns:
         int: answer
     """
-    if s[0].isdigit():
-        if s[1].isdigit():
-            if s[2].isdigit():
-                if s[3].isdigit():
-                   if s[4].isdigit(): 
-                    return len(s[4])+len(s[3])+ len(s[2])+ len(s[1])+len(s[0]) 
-    if s[0].isdigit():
-        if s[1].isdigit():
-            if s[2].isdigit():
-                if s[3].isdigit():
-                   if s[4].isalpha(): 
-                    return len(s[3])+ len(s[2])+ len(s[1])+len(s[0])         
-    if s[0].isdigit():
-        if s[1].isdigit():
-            if s[2].isdigit():
-                if s[3].isalpha():
-                   if s[4].isdigit(): 
-                    return len(s[4])+ len(s[2])+ len(s[1])+len(s[0]) 
-    if s[0].isdigit():
-        if s[1].isdigit():
-            if s[2].isalpha():
-                if s[3].isdigit():
-                   if s[4].isdigit(): 
-                    return len(s[4])+len(s[3])+ len(s[1])+len(s[0])       
-    if s[0].isdigit():
-        if s[1].isalpha():
-            if s[2].isdigit():
-                if s[3].isdigit():
-                   if s[4].isdigit(): 
-                    return len(s[4])+len(s[3])+ len(s[2])+len(s[0])   
-    if s[0].isalpha():
-        if s[1].isdigit():
-            if s[2].isdigit():
-                if s[3].isdigit():
-                   if s[4].isdigit(): 
-                    return len(s[4])+len(s[3])+ len(s[2])+ len(s[1])                                        
-    if s[0].isalpha():
-        if s[1].isalpha():
-            if s[2].isalpha():
-                if s[3].isalpha():
-                   if s[4].isalpha(): 
-                    return False      
-print(main('dfgsa'))
+    c=0
+    if s[0]=='0':
+        c+=1
+    if s[0]=='1':
+        c+=1
+    if s[0]=='2':
+        c+=1
+    if s[0]=='3':
+        c+=1
+    if s[0]=='4':
+        c+=1
+    if s[0]=='5':
+        c+=1
+    if s[0]=='6':
+        return 1
+    if s[0]=='7':
+        c+=1
+    if s[0]=='8':
+        c+=1
+    if s[0]=='9':
+        c+=1                       
+    if s[0]=='0':
+        c+=1
+    if s[1]=='1':
+        c+=1
+    if s[1]=='2':
+        c+=1
+    if s[1]=='3':
+        c+=1
+    if s[1]=='4':
+        c+=1
+    if s[1]=='5':
+        c+=1
+    if s[1]=='6':
+        c+=1
+    if s[1]=='7':
+        c+=1
+    if s[1]=='8':
+        c+=1
+    if s[1]=='9':
+        c+=1
+    if s[2]=='0':
+        c+=1
+    if s[2]=='1':
+        c+=1
+    if s[2]=='2':
+        c+=1
+    if s[2]=='3':
+        c+=1
+    if s[2]=='4':
+        c+=1
+    if s[2]=='5':
+        c+=1
+    if s[2]=='6':
+        c+=1
+    if s[2]=='7':
+        c+=1
+    if s[2]=='8':
+        c+=1
+    if s[2]=='9':
+        c+=1
+    if s[3]=='0':
+        c+=1
+    if s[3]=='1':
+        c+=1
+    if s[3]==2:
+        c+=1
+    if s[3]=='3':
+        c+=1
+    if s[3]=='4':
+        c+=1
+    if s[3]=='5':
+        c+=1
+    if s[3]=='6':
+        c+=1
+    if s[3]=='7':
+        c+=1
+    if s[3]=='8':
+        c+=1
+    if s[3]=='9':
+        c+=1
+    if s[4]=='0':
+        c+=1
+    if s[4]=='1':
+        c+=1
+    if s[4]=='2':
+        c+=1
+    if s[4]=='3':
+        c+=1
+    if s[4]=='4':
+        c+=1
+    if s[4]=='5':
+        c+=1
+    if s[4]=='6':
+        c+=1
+    if s[4]=='7':
+        c+=1
+    if s[4]=='8':
+        c+=1
+    if s[4]=='9':
+        c+=1    
+    return c                 
+print(main('3253z'))
