bbc91f1eecf311137123bfd438a14580700cba6f
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-18 13:35:45 +0500

    08

--- Index08.py
+++ Index08.py
@@ -6,5 +6,24 @@ def main(s):
     Returns:
         int: answer
     """
-    return
-        
+    if s[0]=='*' :
+        return 0
+    if s[1]=='*' :
+        return 1
+    if s[2]=='*' :
+        return 2
+    if s[3]=='*' :
+        return 3
+    if s[4]=='*' :
+        return 4               
+    elif s[0]!='*':
+        return False 
+    elif s[1]!='*':
+        return False 
+    elif s[2]!='*':
+        return False 
+    elif s[3]!='*':
+        return False 
+    elif s[4]!='*':
+        return False                   
+print(main('42111'))    
