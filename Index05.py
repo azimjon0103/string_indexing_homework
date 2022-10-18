c02878c2f6a04f4ac49b022e06e5cb6a89172a18
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-18 12:51:47 +0500

    05

--- Index05.py
+++ Index05.py
@@ -6,4 +6,46 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s[0].isdigit():
+        if s[1].isdigit():
+            if s[2].isdigit():
+                if s[3].isdigit():
+                   if s[4].isdigit(): 
+                    return len(s[4])+len(s[3])+ len(s[2])+ len(s[1])+len(s[0]) 
+    if s[0].isdigit():
+        if s[1].isdigit():
+            if s[2].isdigit():
+                if s[3].isdigit():
+                   if s[4].isalpha(): 
+                    return len(s[3])+ len(s[2])+ len(s[1])+len(s[0])         
+    if s[0].isdigit():
+        if s[1].isdigit():
+            if s[2].isdigit():
+                if s[3].isalpha():
+                   if s[4].isdigit(): 
+                    return len(s[4])+ len(s[2])+ len(s[1])+len(s[0]) 
+    if s[0].isdigit():
+        if s[1].isdigit():
+            if s[2].isalpha():
+                if s[3].isdigit():
+                   if s[4].isdigit(): 
+                    return len(s[4])+len(s[3])+ len(s[1])+len(s[0])       
+    if s[0].isdigit():
+        if s[1].isalpha():
+            if s[2].isdigit():
+                if s[3].isdigit():
+                   if s[4].isdigit(): 
+                    return len(s[4])+len(s[3])+ len(s[2])+len(s[0])   
+    if s[0].isalpha():
+        if s[1].isdigit():
+            if s[2].isdigit():
+                if s[3].isdigit():
+                   if s[4].isdigit(): 
+                    return len(s[4])+len(s[3])+ len(s[2])+ len(s[1])                                        
+    if s[0].isalpha():
+        if s[1].isalpha():
+            if s[2].isalpha():
+                if s[3].isalpha():
+                   if s[4].isalpha(): 
+                    return False      
+print(main('dfgsa'))
